7d9cea82f56d385ef3c7fcc22bff5a0478ea5178
xtianaguilar79-hue <xtianaguilar79@gmail.com>
2026-06-08 15:57:20 -0300

    Add files via upload

---
+++ js/supabase-config.js
@@ -0,0 +1,10 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
+// Credenciales de Supabase
+const SUPABASE_URL = 'https://atiojuyzcumlrixxobah.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0aW9qdXl6Y3VtbHJpeHhvYmFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MzU4NDAsImV4cCI6MjA5NjUxMTg0MH0.2GEcdXqa6Bg6tEmXYAxTFQ30Obn8PlL3i2fWezJWzPg';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Email del admin
+export const ADMIN_EMAIL = "aomasjhys@gmail.com";
