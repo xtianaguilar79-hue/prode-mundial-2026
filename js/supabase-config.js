7e99e54ea803fa0564afc6a0e7c7e99882128079
xtianaguilar79-hue <xtianaguilar79@gmail.com>
2026-06-15 11:37:44 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://atiojuyzcumlrixxobah.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0aW9qdXl6Y3VtbHJpeHhvYmFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MzU4NDAsImV4cCI6MjA5NjUxMTg0MH0.2GEcdXqa6Bg6tEmXYAxTFQ30Obn8PlL3i2fWezJWzPg';
+export const SUPABASE_URL = 'https://atiojuyzcumlrixxobah.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0aW9qdXl6Y3VtbHJpeHhvYmFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MzU4NDAsImV4cCI6MjA5NjUxMTg0MH0.2GEcdXqa6Bg6tEmXYAxTFQ30Obn8PlL3i2fWezJWzPg';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 export const ADMIN_EMAIL = "aomasjhys@gmail.com";
