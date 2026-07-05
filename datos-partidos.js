e95d582cc6c4c026bfb426f5a9edea1cebb221c8
xtianaguilar79-hue <xtianaguilar79@gmail.com>
2026-07-05 19:13:42 -0300

    Update datos-partidos.js

--- datos-partidos.js
+++ datos-partidos.js
@@ -188,7 +188,7 @@ export const PARTIDOS_ELIM = [
   { id:"M096", fase:"octavos", local:"Suiza", visit:"Colombia", fecha:"07/07", hora:"17:00", sede:"Vancouver, CAN" },
   { id:"M097", fase:"cuartos", local:"Francia", visit:"Marruecos", fecha:"09/07", hora:"17:00", sede:"Boston, USA" },
   { id:"M098", fase:"cuartos", local:"G93", visit:"G94", fecha:"10/07", hora:"16:00", sede:"Los Angeles, USA" },
-  { id:"M099", fase:"cuartos", local:"G91", visit:"G92", fecha:"11/07", hora:"18:00", sede:"Miami, USA" },
+  { id:"M099", fase:"cuartos", local:"Noruega", visit:"G92", fecha:"11/07", hora:"18:00", sede:"Miami, USA" },
   { id:"M100", fase:"cuartos", local:"G95", visit:"G96", fecha:"11/07", hora:"22:00", sede:"Kansas City, USA" },
   { id:"M101", fase:"semis", local:"G97", visit:"G98", fecha:"14/07", hora:"16:00", sede:"Dallas, USA" },
   { id:"M102", fase:"semis", local:"G99", visit:"G100", fecha:"15/07", hora:"16:00", sede:"Atlanta, USA" },
